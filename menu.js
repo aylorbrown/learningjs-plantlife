7465c5e79a5fb8b27c5810043d4c4c00c143f0f0
Aylor Brown <aylorbrown@gmail.com>
2020-01-11 16:23:28 -0500

    adds styling to the menu

--- menu.js
+++ menu.js
@@ -1,11 +1,13 @@
 const toggleTag = document.querySelector("a.toggle-nav")
 const mainTag = document.querySelector("main")
 
+
 // when i click the toggle tag, toggle a class of open on the main tag 
 // and if its open, make the toggle tag says closed 
 // and if its closed, make the toggle tag say menu 
 toggleTag.addEventListener("click", function () {
     mainTag.classList.toggle("open")
+
 
 
     if (mainTag.classList.contains("open")) {
